f5d8bacc83837b7ba4f177b31c5c4cba653b8842
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2025-02-03 22:36:42 +0300

    IDE found the LSP provider

--- try2/build.gradle.kts
+++ try2/build.gradle.kts
@@ -1,15 +1,27 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
 }
+
 
 group = "com.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaUltimate("2024.3.2.1")
+    }
+}
+
 
 sourceSets {
     main {
@@ -17,15 +29,6 @@ sourceSets {
             srcDirs("src/main/gen")
         }
     }
-}
-
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.2.8")
-    type.set("IU") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
 }
 
 tasks {
@@ -40,7 +43,6 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
     }
 
     signPlugin {
--- try2/src/main/java/myPackage/AstraLspServerSupportProvider.java
+++ try2/src/main/java/myPackage/AstraLspServerSupportProvider.java
@@ -11,6 +11,9 @@ import org.jetbrains.annotations.NotNull;
 public class AstraLspServerSupportProvider implements LspServerSupportProvider {
     @Override
     public void fileOpened(@NotNull Project project, @NotNull VirtualFile file, @NotNull LspServerSupportProvider.LspServerStarter serverStarter) {
+
+        System.out.println("fileOpened: " + file.getExtension());
+
         if (file.getExtension().equals("ac")) {
             serverStarter.ensureServerStarted(new AstraLspServerDescriptor(project, file.getPresentableName()));
         }
@@ -21,11 +24,14 @@ class AstraLspServerDescriptor extends ProjectWideLspServerDescriptor {
 
     public AstraLspServerDescriptor(@NotNull Project project, @NotNull String presentableName) {
         super(project, presentableName);
+
+        System.out.println("description ctor");
     }
 
     @Override
     public @NotNull GeneralCommandLine createCommandLine() throws ExecutionException {
         String serverExePath = "C:/Users/REDIZIT/Documents/GitHub/Astra-Rider-extension/LanguageServer/bin/Debug/net8.0/LanguageServer.exe";
+        System.out.println("create cmd");
         return new GeneralCommandLine(serverExePath, "lsp");
     }
 
